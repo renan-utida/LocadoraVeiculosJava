c970b111dc35bb174e971e89125279776efca7fa
Renan <renandiutida@gmail.com>
2025-04-01 20:01:44 -0300

    Criando a classe veícuÃlo e adicionando alguns métodos

--- src/main/java/Veiculo.java
+++ src/main/java/Veiculo.java
@@ -1,3 +1,12 @@
 public class Veiculo {
+    private String modelo;
+    private double valorDiaria;
 
+    public double calcularCusto(int dias){
+        return dias * valorDiaria;
+    }
+
+    public double aplicarDesconto(int dias){
+        return valorDiaria * 0.9; // Desconto de 10% do valor total
+    }
 }
