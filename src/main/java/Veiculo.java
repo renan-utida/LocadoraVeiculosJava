4db6aa83dc3888f7fa554f82f732f8a3465cbfd7
Renan <renandiutida@gmail.com>
2025-04-01 20:11:58 -0300

    Adicionando mÃetodos de desconto e calcularCusto além de adicionar construtores em Veiculo

--- src/main/java/Veiculo.java
+++ src/main/java/Veiculo.java
@@ -2,11 +2,28 @@ public class Veiculo {
     private String modelo;
     private double valorDiaria;
 
-    public double calcularCusto(int dias){
-        return dias * valorDiaria;
+    // Construtor
+    public Veiculo(String modelo, double valorDiaria){
+        if (valorDiaria < 0){
+            throw new IllegalArgumentException("O valor da diária não pode ser negativo.");
+        }
+        this.modelo = modelo;
+        this.valorDiaria = valorDiaria;
     }
 
-    public double aplicarDesconto(int dias){
-        return valorDiaria * 0.9; // Desconto de 10% do valor total
+    // Metodo para calcular o custo total do aluguel
+    public double calcularCusto(int dias){
+        if (dias < 0) {
+            throw new IllegalArgumentException("O número de dias não pode ser negativo")
+        }
+        double custoTotal = dias * valorDiaria
+        return custoTotal - desconto(dias);
+    }
+
+    public double desconto(int dias){
+        if (dias >= 7){
+            return (dias * valorDiaria) * 0.1; // Desconto de 10% do valor total
+        }
+        return 0;
     }
 }
